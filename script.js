73571a4b4b18c300521a1912200e5a289a50e20f
Kiyotho <somayojha2@gmail.com>
2026-05-11 17:14:27 +0530

    changed and prohibit the ability to enter negative values

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const input = document.getElementById("input")
 const valueLen = document.getElementById("value-len")
 const valueVol = document.getElementById("value-vol")
 const valueMass = document.getElementById("value-mass")
+const err = document.getElementById("err")
+let errTimeout = null
 
 let value = 0
 
@@ -11,16 +13,27 @@ convertBtn.addEventListener("click", function(){
 
     value = Number(input.value)
 
-    const valueFeet = value * 3.28084
-    const valueGallon = value * 0.264172
-    const valuePound = value * 2.20462
+    if(value >= 0){
 
-    const valueMeter = value / 3.28084
-    const valueLiter = value / 0.264172
-    const valueKilo = value / 2.20462
+        const valueFeet = value * 3.28084
+        const valueGallon = value * 0.264172
+        const valuePound = value * 2.20462
 
-    valueLen.textContent = `${value} meters = ${valueFeet.toFixed(2)} feet | ${value} feet = ${valueMeter.toFixed(2)} meters`
-    valueVol.textContent = `${value} liters = ${valueGallon.toFixed(2)} gallons | ${value} gallons = ${valueLiter.toFixed(2)} liters`
-    valueMass.textContent = `${value} kilos = ${valuePound.toFixed(2)} pounds | ${value} pounds = ${valueKilo.toFixed(2)} kilos`
+        const valueMeter = value / 3.28084
+        const valueLiter = value / 0.264172
+        const valueKilo = value / 2.20462
 
+        valueLen.textContent = `${value} meters = ${valueFeet.toFixed(2)} feet | ${value} feet = ${valueMeter.toFixed(2)} meters`
+        valueVol.textContent = `${value} liters = ${valueGallon.toFixed(2)} gallons | ${value} gallons = ${valueLiter.toFixed(2)} liters`
+        valueMass.textContent = `${value} kilos = ${valuePound.toFixed(2)} pounds | ${value} pounds = ${valueKilo.toFixed(2)} kilos`
+    } else {
+
+        err.textContent = "Don't Input Negative Values."
+        clearTimeout(errTimeout)
+
+        errTimeout = setTimeout(function() {
+            err.textContent = ""
+        }, 2000)
+
+    }
 })
